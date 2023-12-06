173d00fcd8a22e63d5835d45590572796b758483
Shoomankhatri <shoomankhatri@gmail.com>
2023-12-06 20:42:55 +0545

    hello5

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -92,6 +92,39 @@ function MyState(props) {
   }, []);
 
 
+
+  const edithandle = (item) => {
+    setProducts(item)
+  }
+  // update product
+  const updateProduct = async (item) => {
+    setLoading(true)
+    try {
+      await setDoc(doc(fireDb, "products", products.id), products);
+      toast.success("Product Updated successfully")
+      getProductData();
+      setLoading(false)
+      window.location.href = '/dashboard'
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+    setProducts("")
+  }
+
+  const deleteProduct = async (item) => {
+
+    try {
+      setLoading(true)
+      await deleteDoc(doc(fireDb, "products", item.id));
+      toast.success('Product Deleted successfully')
+      setLoading(false)
+      getProductData()
+    } catch (error) {
+      // toast.success('Product Deleted Falied')
+      setLoading(false)
+    }
+  }
   return (
     <MyContext.Provider value={{ 
       mode, toggleMode, loading,setLoading,
